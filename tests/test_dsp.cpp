92b21a10105b64221a2e7ff108151474ae3e0a3c
chairbender <kwhipke1@gmail.com>
2025-03-19 21:10:33 -0700

    very basic starting point for actual test of a DSP method

---
+++ tests/test_dsp.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "../include/dsp.hpp"
+
+TEST(FFTTest, Forward) {
+	// TODO: setup vectors
+	// TODO: invoke
+	// TODO: compare expected / actual
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
